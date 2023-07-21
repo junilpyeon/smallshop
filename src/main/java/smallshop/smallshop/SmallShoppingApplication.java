007a8861a858cc441388fceb9b9f599f7e1fc365
junilpyeon <dreamerjunil@gmail.com>
2023-07-21 23:18:38 +0900

    Initial commit

---
+++ src/main/java/smallshop/smallshop/SmallShoppingApplication.java
@@ -0,0 +1,13 @@
+package smallshop.smallshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallShoppingApplication.class, args);
+	}
+
+}
